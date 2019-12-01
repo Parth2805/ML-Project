89435ec84e0764bde3ad2ee6e32788ad05f4a9de
jil <mehtajil131@gmail.com>
2019-12-01 10:43:47 -0500

    Adding structure for classification and regression

--- src/main.py
+++ src/main.py
@@ -1,9 +1,34 @@
 from pip._vendor.distlib.compat import raw_input
+import classification
+import regression
 
 
-user_response = raw_input("Enter Y for Yes and N for No\n")
+'''user_response = raw_input("Enter Y for Yes and N for No\n")
 
 if(user_response.__eq__('Y')):
     print("You said Yes")
 else:
-    print("You said No")
+    print("You said No")'''
+
+
+
+class main():
+
+    def start(self):
+        user_response = raw_input("Press 1 to get preloaded models and 2 to run the classification and regression \n")
+
+        if(user_response.__eq__('1')):
+            print("Running Preloaded models\n")
+        else:
+            print("Running classifier and Regression tests\n")
+            classifier = classification.class_classification()
+            classifier.get_classifier()
+
+            regressor = regression.class_regression()
+            regressor.get_regressor()
+
+
+
+start1 = main()
+start1.start()
+
