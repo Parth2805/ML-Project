9b4d4fcf6cbeb238a42f4830d042cf2d7370e779
jil <mehtajil131@gmail.com>
2019-12-01 13:49:39 -0500

    regression

--- src/regression.py
+++ src/regression.py
@@ -2,4 +2,14 @@ class class_regression:
     '''Contains all the regression logic'''
 
     def get_regressor(self):
-        print('into regressor')
+        print('Running regressors for the following datasets: \n')
+        self.WineQuality()
+        self.Communities_Crime()
+        self.QSAR_aquatic_toxicity()
+        self.Parkinson_Speech()
+        self.Facebook_metrics()
+        self.Bike_Sharing()
+        self.Student_Performance()
+        self.Concrete_Compressive_Strength()
+        self.SGEMM_GPU_kernel_performance()
+        self.Merck_Molecular_Activity_Challenge()
