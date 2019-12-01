2d916830b7076c488efd424bda516d9244f797b9
Rajan <erajan96@yahoo.com>
2019-12-01 08:51:03 -0500

    added .gitignore

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
-from pip._vendor.distlib.compat import raw_input
+import pip._vendor.distlib.compat
 
 
 user_response = raw_input("Enter Y for Yes and N for No\n")
 
-if(user_response.__eq__('Y')):
+if user_response.__eq__('Y'):
     print("You said Yes")
 else:
     print("You said No")
