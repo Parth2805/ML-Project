035b5089f0f9f696e97a96f0d94fdd8fbaedb959
Rajan <erajan96@yahoo.com>
2019-12-03 22:27:26 -0500

    merck | load pretrained model

--- src/regression.py
+++ src/regression.py
@@ -58,12 +58,32 @@ class class_regression:
         pickle.dump(model.best_params_, open(RESULTS_FOR_DEMO + "%sBestParams.sav" % name, 'wb'))
         plot.plot_learning_curve(model.best_estimator_, name + " Learning Curve", X_train, y_train, (0.5, 1.01), cv=cv)
 
-    def load_pretrained_models(self, name):
+    def load_pretrained_models(self, name, X_train, y_train, X_test, y_test, cv):
         print("Loading PreTrained model: ", name)
         model = pickle.load(PRETRAINED_MODEL + name + ".sav")
-        print("Testing Accuracy: ", model.score(X_test, y_test))
-        print("Training Accuracy: ", model.score(X_train, y_train))
+        print("Mean Squared Error: ",
+              metrics.mean_squared_error(y_test, model.predict(X_test)))
+        print("R2 Score: ", metrics.r2_score(y_test, model.predict(X_test)))
         plot.plot_learning_curve(model, name + " Learning Curve", X_train, y_train, (0.5, 1.01), cv=cv)
+
+    def merck_model(self, act2, act4, X_test_act2, y_test_act2, X_test_act4, y_test_act4, name):
+        print(name)
+        act2_predict = act2.predict(X_test_act2)
+        act4_predict = act4.predict(X_test_act4)
+        act2_r2 = metrics.r2_score(y_test_act2, act2_predict)
+        act4_r2 = metrics.r2_score(y_test_act4, act4_predict)
+
+        print("R2 Score: act2 {0}: & act4: {1} ".format(act2_r2, act4_r2))
+        print("Average R2 Score: ", (act2_r2 + act4_r2) / 2)
+
+        pearson_act2 = Stats.pearsonr(act2_predict, y_test_act2)
+        pearson_act4 = Stats.pearsonr(act4_predict, y_test_act4)
+        print("PearsonR: act2 {0}: & act4: {1} ".format(pearson_act2, pearson_act4))
+
+        pickle.dump(act2, open(RESULTS_FOR_DEMO + name + "BestModelACT2.sav", 'wb'))
+        pickle.dump(act2.get_params, open(RESULTS_FOR_DEMO + name + "BestParamsACT2.sav", 'wb'))
+        pickle.dump(act4, open(RESULTS_FOR_DEMO + name + "BestModelACT4.sav", 'wb'))
+        pickle.dump(svr_act4.get_params, open(RESULTS_FOR_DEMO + name + "BestParamsACT2.sav", 'wb'))
 
     def get_regressor(self, userResponse):
         print('Running regressors for the following datasets: \n')
@@ -74,9 +94,9 @@ class class_regression:
         # self.Facebook_metrics()
         # self.Bike_Sharing()
         # self.Student_Performance()
-        self.Concrete_Compressive_Strength(userResponse)
+        # self.Concrete_Compressive_Strength(userResponse)
         # self.SGEMM_GPU_kernel_performance()
-        # self.Merck_Molecular_Activity_Challenge(userResponse)
+        self.Merck_Molecular_Activity_Challenge(userResponse)
 
     def WineQuality(self):
         print('Running Regression for 1.WineQuality dataset')
@@ -199,21 +219,20 @@ class class_regression:
 
     def Concrete_Compressive_Strength(self, userResponse):
         print('Running Regression for 8.Concrete_Compressive_Strength dataset')
+        df = pd.read_excel(DATASETS + "Concrete_Data.xls")
+        df.columns = ["Cement", "Blast Furnace Slag", "Fly Ash", "Water", "Superplasticizer", "Coarse Aggregate",
+                      "FineAggregate", "Age", " Concrete Strength"]
+        train_data, test_data = train_test_split(df, test_size=0.2, random_state=0)
+        X_train, y_train = train_data.iloc[:, :-1], train_data.iloc[:, -1]
+        X_test, y_test = test_data.iloc[:, :-1], test_data.iloc[:, -1]
+        scaler = Preprocessing.StandardScaler()
+        scaler.fit(X_train)
+        X_train_scaled = scaler.transform(X_train)
+        X_train_scaled = pd.DataFrame(X_train_scaled)
+
+        X_test_scaled = scaler.transform(X_test)
+        X_test_scaled = pd.DataFrame(X_test_scaled)
         if userResponse is "2":
-            df = pd.read_excel(DATASETS + "Concrete_Data.xls")
-            df.columns = ["Cement", "Blast Furnace Slag", "Fly Ash", "Water", "Superplasticizer", "Coarse Aggregate",
-                          "FineAggregate", "Age", " Concrete Strength"]
-            train_data, test_data = train_test_split(df, test_size=0.2, random_state=0)
-            X_train, y_train = train_data.iloc[:, :-1], train_data.iloc[:, -1]
-            X_test, y_test = test_data.iloc[:, :-1], test_data.iloc[:, -1]
-            scaler = Preprocessing.StandardScaler()
-            scaler.fit(X_train)
-            X_train_scaled = scaler.transform(X_train)
-            X_train_scaled = pd.DataFrame(X_train_scaled)
-
-            X_test_scaled = scaler.transform(X_test)
-            X_test_scaled = pd.DataFrame(X_test_scaled)
-
             # SVM
             param_grid = {'C': [0.1, 1, 10, 100, 1000],
                           'gamma': [1e-3, 1e-2, 1e-1, 10]
@@ -221,7 +240,7 @@ class class_regression:
             self.grid_search_cv(SVM.SVR(kernel='rbf'), param_grid, X_train_scaled, y_train, X_test_scaled, y_test,
                                 "ConcreteSVM")
 
-            LR
+            # LR
             lr_model = linear.LinearRegression().fit(X_train_scaled, y_train)
             print("Linear Regression Mean Squared Error: ",
                   metrics.mean_squared_error(y_test, lr_model.predict(X_test_scaled)))
@@ -276,28 +295,122 @@ class class_regression:
                                 params_grid, X_train_scaled, y_train, X_test_scaled, y_test, "ConcreteDTC")
         else:
             # SVM
-            self.load_pretrained_models("SVR_concrete_model")
+            self.load_pretrained_models("SVR_concrete_model", X_train_scaled, y_train, X_test_scaled, y_test, 5)
 
             # DTC
-            self.load_pretrained_models("decsion_concrete_tree_model")
+            self.load_pretrained_models("decsion_concrete_tree_model", X_train_scaled, y_train, X_test_scaled, y_test,
+                                        5)
 
             # RFC
-            self.load_pretrained_models("random_concrete_forest_model")
+            self.load_pretrained_models("random_concrete_forest_model", X_train_scaled, y_train, X_test_scaled, y_test,
+                                        5)
 
             # LR
-            self.load_pretrained_models("Linear_regression_model")
+            self.load_pretrained_models("Linear_regression_model", X_train_scaled, y_train, X_test_scaled, y_test, 5)
 
             # Adaboost
-            self.load_pretrained_models("AdaBoost_concrete_model")
+            self.load_pretrained_models("AdaBoost_concrete_model", X_train_scaled, y_train, X_test_scaled, y_test, 5)
 
             # GNB
-            self.load_pretrained_models("gaussian_concrete_model")
+            self.load_pretrained_models("gaussian_concrete_model", X_train_scaled, y_train, X_test_scaled, y_test, 5)
 
             # MLP
-            self.load_pretrained_models("neural_network_concrete_model")
+            self.load_pretrained_models("neural_network_concrete_model", X_train_scaled, y_train, X_test_scaled, y_test,
+                                        5)
 
     def SGEMM_GPU_kernel_performance(self):
         print('Running Regression for 9.SGEMM_GPU_kernel_performance dataset')
 
     def Merck_Molecular_Activity_Challenge(self, userResponse):
         print('Running Regression for 10.Merck_Molecular_Activity_Challenge dataset')
+        act2 = np.load(DATASETS + "ac2_cache.npz")
+        # print(act2.files)
+        act4 = np.load(DATASETS + "ac4_cache.npz")
+
+        X_act2, y_act2 = act2['arr_0'], act2['arr_1']
+        X_act4, y_act4 = act4['arr_0'], act4['arr_1']
+        X_train_act2, X_test_act2, y_train_act2, y_test_act2 = train_test_split(X_act2, y_act2, test_size=0.2,
+                                                                                random_state=0)
+        X_train_act4, X_test_act4, y_train_act4, y_test_act4 = train_test_split(X_act4, y_act4, test_size=0.2,
+                                                                                random_state=0)
+        if userResponse is "2":
+            # LR
+            lr_model_act2 = linear.LinearRegression().fit(X_train_act2, y_train_act2)
+            print("act2 done")
+            lr_model_act4 = linear.LinearRegression().fit(X_train_act4, y_train_act4)
+
+            self.merck_model(lr_model_act2, lr_model_act4, X_test_act2, y_test_act2, X_test_act4, y_test_act4, "LR")
+
+            # SVR
+            svr_act2 = SVM.SVR().fit(X_train_act2, y_train_act2)
+            svr_act4 = SVM.SVR().fit(X_train_act4, y_train_act4)
+            self.merck_model(svr_act2, svr_act4, X_test_act2, y_test_act2, X_test_act4, y_test_act4, "SVR")
+
+            # DTC
+            dct_act2 = Tree.DecisionTreeRegressor(random_state=0).fit(X_train_act2, y_train_act2)
+            dct_act4 = Tree.DecisionTreeRegressor(random_state=0).fit(X_train_act4, y_train_act4)
+            self.merck_model(dct_act2, dct_act4, X_test_act2, y_test_act2, X_test_act4, y_test_act4, "DTC")
+
+            # RFC
+            rfr_act2 = Ensemble.RandomForestRegressor(random_state=0).fit(X_train_act2, y_train_act2)
+            rfr_act4 = Ensemble.RandomForestRegressor(random_state=0).fit(X_train_act4, y_train_act4)
+            self.merck_model(rfr_act2, rfr_act4, X_test_act2, y_test_act2, X_test_act4, y_test_act4, "RFC")
+
+            # Adaboost
+            ada_act2 = Ensemble.AdaBoostRegressor(random_state=0).fit(X_train_act2, y_train_act2)
+            ada_act4 = Ensemble.AdaBoostRegressor(random_state=0).fit(X_train_act4, y_train_act4)
+            self.merck_model(ada_act2, ada_act4, X_test_act2, y_test_act2, X_test_act4, y_test_act4, "Adaboost")
+
+            # GPR
+            gau_act2 = Gaussian.GaussianProcessRegressor(optimizer="fmin_l_bfgs_b", random_state=0).fit(X_train_act2,
+                                                                                                        y_train_act2)
+            gau_act4 = Gaussian.GaussianProcessRegressor(optimizer="fmin_l_bfgs_b", random_state=0).fit(X_train_act4,
+                                                                                                        y_train_act4)
+            self.merck_model(gau_act2, gau_act4, X_test_act2, y_test_act2, X_test_act4, y_test_act4, "GPR")
+
+            # MLP
+            param_grid = {
+                "activation": ['relu', 'tanh'],
+                "learning_rate_init": Stats.reciprocal(0.001, 0.1),
+                "hidden_layer_sizes": [(512, 256, 128, 64, 32), (128, 64, 32, 16), (64, 32, 16)]
+            }
+
+            mlp_act2 = NN.MLPRegressor(solver='adam', n_iter_no_change=10, momentum=0.9, learning_rate='adaptive',
+                                       random_state=0, verbose=True, warm_start=True, early_stopping=True)
+            nn_act2 = model_select.RandomizedSearchCV(mlp_act2, param_grid, cv=5, scoring='r2', verbose=5,
+                                                      error_score='np.nan').fit(X_train_act2, y_train_act2)
+            print("act2 done")
+            mlp_act4 = NN.MLPRegressor(solver='adam', n_iter_no_change=10, momentum=0.9, learning_rate='adaptive',
+                                       random_state=0, verbose=True, warm_start=True, early_stopping=True)
+            nn_act4 = RandomizedSearchCV(mlp_act4, param_grid, cv=5, scoring='r2', verbose=5, error_score='np.nan').fit(
+                X_train_act4, y_train_act4)
+            self.merck_model(nn_act2.best_estimator_, nn_act4.best_estimator_, X_test_act2, y_test_act2, X_test_act4,
+                             y_test_act4, "MLP")
+        else:
+            act2 = pickle.load(PRETRAINED_MODEL + "Linear_act2_regression_model.sav")
+            act4 = pickle.load(PRETRAINED_MODEL + "Linear_act4_regression_model.sav")
+            self.merck_model(act2, act4, X_test_act2, y_test_act2, X_test_act4, y_test_act4, "LR")
+
+            act2 = pickle.load(PRETRAINED_MODEL + "svr_act2_model.sav")
+            act4 = pickle.load(PRETRAINED_MODEL + "svr_act4_model.sav")
+            self.merck_model(act2, act4, X_test_act2, y_test_act2, X_test_act4, y_test_act4, "SVR")
+
+            act2 = pickle.load(PRETRAINED_MODEL + "dct_act2_model.sav")
+            act4 = pickle.load(PRETRAINED_MODEL + "dct_act4_model.sav")
+            self.merck_model(act2, act4, X_test_act2, y_test_act2, X_test_act4, y_test_act4, "DTC")
+
+            act2 = pickle.load(PRETRAINED_MODEL + "rfr_act2_model.sav")
+            act4 = pickle.load(PRETRAINED_MODEL + "rfr_act4_model.sav")
+            self.merck_model(act2, act4, X_test_act2, y_test_act2, X_test_act4, y_test_act4, "RFC")
+
+            act2 = pickle.load(PRETRAINED_MODEL + "ada_act2_model.sav")
+            act4 = pickle.load(PRETRAINED_MODEL + "ada_act4_model.sav")
+            self.merck_model(act2, act4, X_test_act2, y_test_act2, X_test_act4, y_test_act4, "Adaboost")
+
+            act2 = pickle.load(PRETRAINED_MODEL + "gau_act2_model.sav")
+            act4 = pickle.load(PRETRAINED_MODEL + "gau_act4_model.sav")
+            self.merck_model(act2, act4, X_test_act2, y_test_act2, X_test_act4, y_test_act4, "GPR")
+
+            act2 = pickle.load(PRETRAINED_MODEL + "mlp_act2_random_model.sav")
+            act4 = pickle.load(PRETRAINED_MODEL + "mlp_act4_random_model.sav")
+            self.merck_model(act2, act4, X_test_act2, y_test_act2, X_test_act4, y_test_act4, "MLP")
