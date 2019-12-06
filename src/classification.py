daa70bb7a59f610533eabd9d1a5f6dca0b16c281
username <bitbucketEmail>
2019-12-05 20:59:56 -0500

    Learning Curve Updated | Pretrained model Tested

--- src/classification.py
+++ src/classification.py
@@ -54,12 +54,11 @@ class class_classification:
         pickle.dump(model.best_params_, open(RESULTS_FOR_DEMO + "%sBestParams.sav" % name, 'wb'))
         plot.plot_learning_curve(model.best_estimator_, name + " Learning Curve", X_train, y_train, (0.5, 1.01), cv=cv)
 
-    def load_pretrained_models(self, name, X_train, y_train, X_test, y_test, cv):
+    def load_pretrained_models(self, name, X_train, y_train, X_test, y_test):
         print("Loading PreTrained model: ", name)
         model = pickle.load(open(PRETRAINED_MODEL + name + ".sav", 'rb'))
         print("Testing Accuracy: ", model.score(X_test, y_test))
         print("Training Accuracy: ", model.score(X_train, y_train))
-        plot.plot_learning_curve(model, name + " Learning Curve", X_train, y_train, (0.5, 1.01), cv=cv)
 
     def run_classifier(self, userResponse):
         print('Running classifiers for the following datasets: \n')
@@ -874,33 +873,28 @@ class class_classification:
 
         else:
             # ADABOOST
-            self.load_pretrained_models("StatlogAustralianABCModel", X_train, y_train, X_test, y_test, 3)
+            self.load_pretrained_models("StatlogAustralianABCModel", X_train, y_train, X_test, y_test)
 
             # DTC
-            self.load_pretrained_models("StatlogAustralianDCTModel", X_train, y_train, X_test, y_test, 5)
+            self.load_pretrained_models("StatlogAustralianDCTModel", X_train, y_train, X_test, y_test)
 
             # GNB
-            self.load_pretrained_models("StatlogAustralianGNBModel", X_train, y_train, X_test, y_test,
-                                        5)
+            self.load_pretrained_models("StatlogAustralianGNBModel", X_train, y_train, X_test, y_test)
 
             # KNN
-            self.load_pretrained_models("StatlogAustralianKNNModel", X_train, y_train, X_test,
-                                        y_test, 3)
+            self.load_pretrained_models("StatlogAustralianKNNModel", X_train, y_train, X_test, y_test)
 
             # LR
-            self.load_pretrained_models("StatlogAustralianLRCModel", X_train, y_train, X_test,
-                                        y_test, 3)
+            self.load_pretrained_models("StatlogAustralianLRCModel", X_train, y_train, X_test, y_test)
 
             # MLP
-            self.load_pretrained_models("StatlogAustralianMLPModel", X_train, y_train, X_test, y_test,
-                                        3)
+            self.load_pretrained_models("StatlogAustralianMLPModel", X_train, y_train, X_test, y_test)
 
             # RF
-            self.load_pretrained_models("StatlogAustralianRFCModel", X_train, y_train, X_test, y_test,
-                                        5)
+            self.load_pretrained_models("StatlogAustralianRFCModel", X_train, y_train, X_test, y_test)
 
             # SVM
-            self.load_pretrained_models("StatlogAustralianSVMModel", X_train, y_train, X_test, y_test, 5)
+            self.load_pretrained_models("StatlogAustralianSVMModel", X_train, y_train, X_test, y_test)
 
     def Statlog_German(self):
         print('Running classification for 5.Statlog German dataset')
@@ -1551,33 +1545,28 @@ class class_classification:
             self.grid_search_cv(GaussianNB(), param_grid, X_train, y_train, X_test, y_test, "YeastGNN", 5)
         else:
             # ADABOOST
-            self.load_pretrained_models("YeastAdaModel", X_train, y_train, X_test, y_test, 3)
+            self.load_pretrained_models("YeastAdaModel", X_train, y_train, X_test, y_test)
 
             # DTC
-            self.load_pretrained_models("YeastGNNModel", X_train, y_train, X_test, y_test, 5)
+            self.load_pretrained_models("YeastGNNModel", X_train, y_train, X_test, y_test)
 
             # KNN
-            self.load_pretrained_models("YeastKNNModel", X_train, y_train, X_test, y_test,
-                                        5)
+            self.load_pretrained_models("YeastKNNModel", X_train, y_train, X_test, y_test)
 
             # LR
-            self.load_pretrained_models("YeastLRModel", X_train, y_train, X_test,
-                                        y_test, 3)
+            self.load_pretrained_models("YeastLRModel", X_train, y_train, X_test, y_test)
 
             # NN
-            self.load_pretrained_models("YeastNNModel", X_train, y_train, X_test,
-                                        y_test, 3)
+            self.load_pretrained_models("YeastNNModel", X_train, y_train, X_test,y_test)
 
             # RF
-            self.load_pretrained_models("YeastRFModel", X_train, y_train, X_test, y_test,
-                                        3)
+            self.load_pretrained_models("YeastRFModel", X_train, y_train, X_test, y_test)
 
             # SVM
-            self.load_pretrained_models("YeastSVMModel", X_train, y_train, X_test, y_test,
-                                        5)
+            self.load_pretrained_models("YeastSVMModel", X_train, y_train, X_test, y_test)
 
             # DCT
-            self.load_pretrained_models("YeastDCTModel", X_train, y_train, X_test, y_test, 5)
+            self.load_pretrained_models("YeastDCTModel", X_train, y_train, X_test, y_test)
 
     def Thoracic_Surgery_Data(self):
         print('Running classification for 9.Thoracic Surgery Data dataset')
